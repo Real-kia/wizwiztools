67bda128996e9f620388ad0cd3d7bff3664c2841
real_kia <65906956+Real-kia@users.noreply.github.com>
2023-06-06 19:37:12 +0430

    changed autobackup
    it will download the backup
    then save it  in /var/www/html/wizwizxui-timebot
    and change the cron setting to run the code from there

--- autobackup.sh
+++ autobackup.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-
+#made by Real-kia :D
 # Check if crontab is installed, install if not present
 if ! command -v crontab &> /dev/null; then
     echo "Crontab is not installed. Installing now..."
     sudo apt-get update
     sudo apt-get install cron -y
 fi
+
+# Define the target directory for the backup script
+backup_directory="/var/www/html/wizwizxui-timebot"
+
+# Download the backupwizwiz.sh script
+curl -sL -o "$backup_directory/backupwizwiz.sh" https://raw.githubusercontent.com/Real-kia/wizwiztools/main/backupwizwiz.sh
+
+# Make the script executable
+chmod +x "$backup_directory/backupwizwiz.sh"
 
 # Ask user for the number of hours
 read -p "Enter the number of hours (e.g., 1, 2, 3): " hours
@@ -14,16 +23,12 @@ read -p "Enter the number of hours (e.g., 1, 2, 3): " hours
 cron_file=$(mktemp)
 
 # Append the new crontab entry to the temporary file
-sleep 0.1
-echo "0 */$hours * * * bash <(curl -s https://raw.githubusercontent.com/Real-kia/wizwiztools/main/backupwizwiz.sh)" > "$cron_file"
-
+echo "0 */$hours * * * bash $backup_directory/backupwizwiz.sh" > "$cron_file"
 
 # Merge the existing crontab entries with the new entry
-sleep 0.1
 crontab -l >> "$cron_file"
-sleep 0.1
 crontab "$cron_file"
-sleep 0.1
+
 # Clean up the temporary file
 rm "$cron_file"
 
